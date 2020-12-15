ae538b5aa5dd73f5c5c5e05b98388b98b7686a7e
HelixPy <67702073+HelixPy@users.noreply.github.com>
2020-12-15 10:30:08 +0100

    More Updates to the GUI

--- comvis.py
+++ comvis.py
@@ -12,15 +12,17 @@ from datetime import datetime
 
 window = tkinter.Tk()
 window.title("OpenCV Project")
-window.geometry("1000x500")
+window.geometry("1100x500")
 window.config(bg="white")
-container=tkinter.Frame(window, width=2200, height=500)
+container=tkinter.Frame(window)
 container.config(bg="white")
 frame_1 = tkinter.Frame(container) #namelabel
 frame_2 = tkinter.Frame(container) #slt_btn for select photo
 frame_3 = tkinter.Frame(container) #options menu
 frame_4 = tkinter.Frame(container) #mnp btn for image manipulation
-frame_6 = tkinter.Frame(window)#image frame
+frame_6 = tkinter.Frame(window)#image contianer
+frame_5a = tkinter.Frame(frame_6) #actual image
+frame_5b = tkinter.Frame(frame_6) #altered image
 now = datetime.now()
 
 def packs():
@@ -29,7 +31,9 @@ def packs():
     frame_2.pack(padx=0, pady=10) #select photo
     frame_3.pack(padx=0, pady=10) #drop down menu
     frame_4.pack(padx=0, pady=60) #manipulate button
-    frame_6.pack(side="right", padx=20, pady=20, expand = "yes")
+    frame_6.pack(side="right", padx=10, pady=10, expand = "yes")
+    frame_5a.pack(side="left", expand="yes")
+    frame_5b.pack(side="right", expand="yes")
 
 welcome = tkinter.Label(master=window,text = F"Welcome Dear! Today is {str(date.today())} and the time is {now.strftime('%H:%M:%S')}",fg="purple", bg="white", width=70)
 welcome.config(font=("courier", 13))
@@ -38,62 +42,67 @@ namelabel = tkinter.Label(master=frame_1, text="Manipulate Image Here", bg="whit
 namelabel.config(font=("courier", 17))
 namelabel.pack()
 
-cv_img = None
 def get_img():
     global cv_img
     global image_lbl
     global photo
-    filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetype=(("png files","*.png"),("all files","*.*")))
+    filename = filedialog.askopenfilename(initialdir="Desktop", title="Select A File", filetype=(("png files","*.png"),("all files","*.*")))
     photo = tkinter.PhotoImage(file=f"{filename}")
-    image_lbl = tkinter.Label(frame_6,image=photo)
-    frame_6.image=photo
+    image_lbl = tkinter.Label(frame_5a,image=photo)
+    frame_5a.image=photo
     image_lbl.pack(fill="both", expand="yes")
     #conversion
     pil_img=Image.open(f"{filename}")
     numpy_image=np.array(pil_img)
-    # cv_img = cv.cvtColor(np.float32(numpy_image), cv.COLOR_RGB2BGR)
-    # cv_img = np.float32(numpy_image)
-    # print(pil_img)
     cv_img = cv.imread(filename)
-
 slt_btn = tkinter.Button(master=frame_2, text = "Select Photo", bg = "white", fg="black", width=20, height=2,command=get_img)
 slt_btn.pack()
 
 def del_img():
     image_lbl.config(image="")
     image_lbl.image=" "
-del_btn = tkinter.Button(master=frame_6, text = "Remove Photo", bg = "purple", fg="white", width=18, height=2,command=del_img)
+del_btn = tkinter.Button(master=frame_5a, text = "Remove Photo", bg = "purple", fg="white", width=18, height=2,command=del_img)
 del_btn.pack(side = "bottom")
 
-img_opt = ["Sketch", "Sharpen", "Blur", "Grayscale", "Rotate", "Select Technique"]
+img_opt = ["Sketch", "Blur", "Grayscale", "Rotate", "Erode", "Select Technique"]
 techniq = ttk.Combobox(frame_3, values=img_opt)
 techniq.config(width=25, height=6)
 techniq.current(5)
 techniq.pack()
 
-# get_img()
-# print(cv_img)
-
 def manip():
-    #cv_image = cv.cvtColor(np.array(photo), cv.COLOR_RGB2BGR)
+    global alt_Img_lbl
+    global alt_img
     if techniq.get() == "Select Technique":
         messagebox.showinfo("Technique Error", "Please select one of the techniques to manipulate e.g Grayscale.")
     elif techniq.get() == "Rotate":
-        # cv.namedWindow("Rotate", cv.WINDOW_AUTOSIZE)
-        # cv_imgs = cv.resize(cv_img, (960, 540))
-        rotate = cv.rotate(cv_img, cv.ROTATE_90_CLOCKWISE)
-        cv.imshow("Rotate", cv_img)
+        alt_img = cv.rotate(cv_img, cv.ROTATE_90_CLOCKWISE)
     elif techniq.get() == "Grayscale":
-        grayscale = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
-        cv.imshow("Grayscale", grayscale)
-    elif techniq.get() == "Sharpen":
-        pass
+        alt_img = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
+    elif techniq.get() == "Erode":
+        kernel = np.ones((6, 6), np.uint8)
+        alt_img = cv.erode(cv_img, kernel, cv.BORDER_REFLECT) 
     elif techniq.get() == "Blur":
-        blur = cv.blur(cv_img, (10, 10))
-        cv.imshow("Blur", blur)
-    elif techniq.get() == "sketch":
-        pass
+        alt_img = cv.blur(cv_img, (10, 10))
+    elif techniq.get() == "Sketch":
+        alt_img = cv.Canny(cv_img,50,100)
+    else:
+        messagebox.showinfo("Technique Error", "Sorry, the technique you clicked is not yet activated")
+    alt_img = cv.cvtColor(alt_img, cv.COLOR_BGR2RGB)
+    tk_img = Image.fromarray(alt_img)
+    tk_img = ImageTk.PhotoImage(tk_img)
+    alt_Img_lbl = tkinter.Label(frame_5b,image=tk_img)
+    frame_5b.image=tk_img
+    alt_Img_lbl.pack()
+
 mnp_btn = tkinter.Button(master=frame_4, text = "Manipulate", bg = "purple", fg="white", width=16, height=2, command=manip)
 mnp_btn.pack()
+
+def clear_eff():
+    alt_Img_lbl.config(image="")
+    alt_Img_lbl.image=" "
+clr_btn = tkinter.Button(master=frame_5b, text = "Clear Effect", bg = "purple", fg="white", width=18, height=2,command=clear_eff)
+clr_btn.pack(side = "bottom")
+
 packs()
 window.mainloop()
